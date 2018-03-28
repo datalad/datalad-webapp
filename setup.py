fab5bd7ba7daaa97dc0f7c639d4f5a3521403eea
Michael Hanke <michael.hanke@gmail.com>
2018-03-28 17:24:36 +0200

    Straight port from https://github.com/datalad/datalad/pull/2212

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(
     # basic project properties can be set arbitrarily
-    name="dmhelloworld",
+    name="datalad_webapp",
     author="The DataLad Team and Contributors",
     author_email="team@datalad.org",
     version='0.1',
-    description="demo DataLad module package",
+    description="DataLad extension for exposing commands via a web request API",
     # datalad command suite specs from here
     install_requires=[
         # in general datalad will be a requirement, unless the datalad extension
@@ -20,8 +20,8 @@ setup(
         'datalad.modules': [
             # the label in front of '=' is the command suite label
             # the entrypoint can point to any symbol of any name, as long it is
-            # valid datalad interface specification (see demo in this module)
-            'hello=dmhelloworld:module_suite',
+            # valid datalad interface specification
+            'webapp=datalad_webapp:module_suite',
         ]
     },
 )
