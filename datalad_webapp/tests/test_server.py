fd17cea51113c0715d0ab1635a3493f8e984c1b0
Michael Hanke <michael.hanke@gmail.com>
2018-04-06 08:13:38 +0200

    TST: Test actual webapp execution (absolutely minimal for now)

---
+++ datalad_webapp/tests/test_server.py
@@ -0,0 +1,13 @@
+from cherrypy.test import helper
+from datalad.api import webapp
+
+
+class SimpleCPTest(helper.CPWebCase):
+    def setup_server():
+        webapp('example_metadata', mode='dry-run')
+
+    setup_server = staticmethod(setup_server)
+
+    def server_ok(self):
+        self.getPage("/")
+        self.assertStatus('200 OK')
